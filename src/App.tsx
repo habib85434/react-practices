a20d4ed17bffc2cacd2020e70f5b23ae27094a25
Habibur Rahman <hreknsjob@gmail.com>
2022-12-04 01:03:02 +0200

    Form validation

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Register } from "./components/basic/register";
+
+function App() {
+  return (
+    <div className="App">
+      <Register />
+    </div>
+  );
+}
+
+export default App;
